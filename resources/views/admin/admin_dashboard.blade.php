c566b893f76491330013dfd808d97ad0fae30c58
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-12-25 22:42:34 +0800

    Added sweet alert

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -164,6 +164,8 @@
 
         <!-- Toaster -->
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <script src="{{ asset('backend/assets/js/code.js') }}"></script>
 
         @if(session()->has('message'))
             <script>
@@ -187,5 +189,41 @@
                 }
             </script>
         @endif
+            <script>
+            $(function(){
+                $(document).on('click', '#delete', function (e) {
+                    e.preventDefault();
+                    var link = $(this).attr("href");
+
+                    Swal.fire({
+                        title: 'Are you sure?',
+                        text: "Delete This Data?",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire({
+                                title: 'Deleted!',
+                                text: 'Your file has been deleted.',
+                                icon: 'success',
+                                showClass: {
+                                    popup: 'animate__animated animate__fadeInDown'
+                                },
+                                hideClass: {
+                                    popup: 'animate__animated animate__fadeOutUp'
+                                },
+                                timer: 1500, // Time to show success message
+                                showConfirmButton: false
+                            }).then(() => {
+                                window.location.href = link;
+                            });
+                        }
+                    });
+                });
+            });
+            </script>
     </body>
 </html>
