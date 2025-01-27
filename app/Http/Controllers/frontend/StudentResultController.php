c613db4f26a4db7950e3f3345b217b3d1606f697
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-01-28 01:18:57 +0800

    Logic for Credential/login

--- app/Http/Controllers/frontend/StudentResultController.php
+++ app/Http/Controllers/frontend/StudentResultController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Result;
 use App\Models\classes;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class StudentResultController extends Controller
 {
@@ -12,5 +14,32 @@ class StudentResultController extends Controller
     {
         $classes = classes::all();
         return view('frontend.index', compact('classes'));
+    }
+
+    public function SearchResult(Request $request)
+    {
+        // dd($request->all());
+        $roll_id = $request->roll_id;
+        $class_id = $request->class_id;
+        $student = Student::where('roll_id', $roll_id)->where('class_id', $class_id)->first();
+
+        if(!$student) {
+            $notification = [
+                'message' => 'Invalid Student Credentials!',                    
+                'alert-type' => 'error'
+            ];
+            return redirect()->back()->with($notification);
+        }
+
+        $result = Result::where('student_id',$student->id)->get();
+        if(count($result) === 0){
+            $notification = array(
+                'message' => 'Sorry Result Not Declared Yet!',                    
+                'alert-type' => 'info'                                             
+            );
+            return redirect()->back()->with($notification);    
+        }
+
+        return view('frontend.student_result', compact('result'));
     }
 }
